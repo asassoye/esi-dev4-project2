09c7a1300c9a921849fc3947bdf33c4e5faee789
Andrew Sassoye <andrew@sassoye.be>
2021-08-16 16:38:31 +0200

    model documentation + warning fixes

--- src/model/model.hpp
+++ src/model/model.hpp
@@ -30,24 +30,98 @@
 #include "model/game/game.hpp"
 
 namespace cpoker::model {
+/**
+ * @brief Type of a map of CardType and count
+ */
 typedef std::map<cards::CardType, unsigned> CardMap;
 
+/**
+ * @brief The model class managed the model of the application
+ */
 class Model : public game::Game {
  public:
+  /**
+   * @brief Returns a vector of player names
+   *
+   * @return Players
+   */
   [[nodiscard]] std::vector<std::string_view> players() const;
+
+  /**
+   * @brief Returns the playing player name
+   *
+   * @return Playing player
+   */
   [[nodiscard]] std::string playingPlayer() const;
+
+  /**
+   * @brief Choose a card in the round
+   *
+   * @param card_type The type of the card
+   */
   void chooseCard(model::cards::CardType card_type);
+
+  /**
+   * @brief Choose a value to announce in the round
+   *
+   * @param card_type The type to announce
+   */
   void chooseValue(model::cards::CardType card_type);
+
+  /**
+   * @brief Choose a receiver in the round
+   *
+   * @param name The name of the receiver player
+   */
   void chooseReceiver(std::string &name);
+
+  /**
+   * @brief Accept the card that is send in the round
+   *
+   * @param guess Guess if the announced value is the truth or a lie
+   */
   void accept(bool guess);
+
+  /**
+   * @brief Transfer the card
+   */
   void transfer();
+
+  /**
+   * @brief Returns the actual RoundStatus
+   *
+   * @return the actual status
+   */
   [[nodiscard]] game::RoundStatus roundStatus() const;
+
+  /**
+   * @brief Returns the announced value
+   *
+   * @return the announced value type
+   */
   [[nodiscard]] model::cards::CardType announcedCard() const;
+
+  /**
+   * @brief Returns the actual board
+   *
+   * @return A mess with all the information of the actual board
+   */
   [[nodiscard]] std::vector<
       std::pair<std::string, std::map<cards::CardType, unsigned>>>
   board() const;
-  std::string looser() const;
 
+  /**
+   * @brief Returns the name of the looser
+   * @return the looser
+   */
+  [[nodiscard]] std::string looser() const;
+
+  /**
+   * @brief Returns a specific player's hand cards
+   *
+   * @param name The name of the player
+   * @return A map with the representation of the player's hand
+   */
   std::map<cards::CardType, unsigned> hand(std::string &name) const;
 };
 }  // namespace cpoker::model
